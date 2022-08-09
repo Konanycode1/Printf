d2f0f244879ec5e19028a8fdf2161aefcc92480f
kouame konan abraham <abrahamkonany@gmail.com>
2022-08-09 15:23:47 +0000

    correction finale

--- functions.c
+++ functions.c
@@ -4,9 +4,9 @@
  * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flag:  Calculates active flags
  * @width: Width
- * @precision: Precision specification
+ * @precis: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
@@ -22,9 +22,9 @@ int print_char(va_list types, char buffer[],
  * print_string - Prints a string
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flag:  Calculates active flags
  * @width: get width.
- * @precision: Precision specification
+ * @precis: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
@@ -77,9 +77,9 @@ int print_string(va_list types, char buffer[],
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flag:  Calculates active flags
  * @width: get width.
- * @precision: Precision specification
+ * @precis: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
@@ -100,9 +100,9 @@ int print_percent(va_list types, char buffer[],
  * print_int - Print int
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flag:  Calculates active flags
  * @width: get width.
- * @precision: Precision specification
+ * @precis: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
@@ -144,9 +144,9 @@ int print_int(va_list types, char buffer[],
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flag:  Calculates active flags
  * @width: get width.
- * @precision: Precision specification
+ * @precis: Precision specification
  * @size: Size specifier
  * Return: Numbers of char printed.
  */
