023fe9e79b6fd348f82da29d0dfdffa52ca607cc
kouame konan abraham <abrahamkonany@gmail.com>
2022-08-09 15:29:43 +0000

    correction finale

--- main.h
+++ main.h
@@ -47,36 +47,36 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 
 int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
+char buffer[], int flag, char flag_ch, int width, int precis, int size);
 
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 
 /* Funcion to print memory address */
 int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+	int flag, int width, int precis, int size);
 
 /* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);
